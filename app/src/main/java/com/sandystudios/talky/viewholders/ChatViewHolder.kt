c8e8a45b48568547c0b135bc7884439a53950bb7
sanidhyakr18 <sanidhyakr18@gmail.com>
2021-11-25 18:18:13 +0530

    gradle upgrade

--- app/src/main/java/com/sandystudios/talky/viewholders/ChatViewHolder.kt
+++ app/src/main/java/com/sandystudios/talky/viewholders/ChatViewHolder.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
 import com.sandystudios.talky.R
 import com.sandystudios.talky.models.Inbox
 import com.sandystudios.talky.utils.formatAsListItem
@@ -21,11 +22,12 @@ class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             findViewById<TextView>(R.id.tv_time).text = item.time.formatAsListItem(context)
             findViewById<TextView>(R.id.tv_title).text = item.name
             findViewById<TextView>(R.id.tv_subtitle).text = item.msg
+            val ivUserImage = findViewById<ShapeableImageView>(R.id.iv_user_image)
             Picasso.get()
                 .load(item.image)
                 .placeholder(R.drawable.default_avatar)
                 .error(R.drawable.default_avatar)
-                .into(findViewById<ImageView>(R.id.iv_user_image))
+                .into(ivUserImage)
             setOnClickListener {
                 onClick.invoke(item.name, item.image, item.from)
             }
